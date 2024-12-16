c3e30eaca09706d4ac90030e04a79dc9e4361f7e
Anik Saha <aniksaha200r@gmail.com>
2024-12-17 01:45:58 +0600

    updated tailwind config

--- Front-End/tailwind.config.js
+++ Front-End/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -5,27 +7,10 @@ export default {
     extend: {},
   },
   plugins: [
-    require("daisyui"),
-    // function ({ addUtilities }) {
-    //   const newUtilities = {
-    //     ".meet-button": {
-    //       backgroundColor: "#ffffff",
-    //       borderRadius: "9999px",
-    //       fontSize: "1.5rem",
-    //       padding: "1rem 1.5rem",
-    //       margin: "0.5rem",
-    //       cursor: "pointer",
-    //       transition: "background-color 0.3s ease",
-    //       "&:hover": {
-    //         backgroundColor: "#f0f0f0",
-    //       },
-    //     },
-    //   };
-
-    //   addUtilities(newUtilities, ["responsive", "hover"]);
-    // },
+    require("@tailwindcss/typography"),
+    daisyui,
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
   ],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
 };
